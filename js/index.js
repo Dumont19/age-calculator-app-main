c4e9d0118ab6a72a9ebc166b35fae31a42a9f559
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2023-06-09 01:19:34 -0300

    Init js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const transformDate = () => {
+    const inputDay = document.querySelector('#day')
+    const inputMonth = document.querySelector('#month')
+    const inputYear = document.querySelector('#year')
+    const btn = document.querySelector('button')
+    const day = document.querySelector('#res-d')
+    const month = document.querySelector('#res-m')
+    const year = document.querySelector('#res-y')
+}
+transformDate()
