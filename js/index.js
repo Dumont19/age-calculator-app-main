a14871afa66a85907b0352519754043084c4a165
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2023-07-24 00:54:31 -0300

    validation

--- js/index.js
+++ js/index.js
@@ -9,7 +9,10 @@ const transformDate = () => {
     const day = document.querySelector('#res-d')
     const month = document.querySelector('#res-m')
     const year = document.querySelector('#res-y')
-    const errorMessage = Array.from(document.querySelectorAll('small'))
+    const errorMessage = Array.from(document.querySelectorAll('.blank-field'))
+    const errorFormatMessage = Array.from(document.querySelectorAll('.format-error'))
+
+    
 
     const checkInputs = () => {
         if (inputDay.value === '') {
@@ -37,9 +40,46 @@ const transformDate = () => {
         }
     }
 
+    const checkFormat = () => {
+        const numberPatternRegex = /[0-9]+/
+        const rangeDayPatternRegex = /^(0[1-9]|3[0-1])$/
+        const rangeMonthPatternRegex = /^(0[1-9]|1[0-2])$/
+        const currentYear = new Date().getFullYear()
+        console.log(rangeDayPatternRegex.test(inputDay.value))
+
+        if (numberPatternRegex.test(inputDay.value) === false || rangeDayPatternRegex.test(inputDay.value) === false) {
+            labelDay.style.color = 'var(--light-red)'
+            inputDay.classList.add('error')
+            errorFormatMessage[0].style.display = 'block'
+        }
+
+        if (numberPatternRegex.test(inputMonth.value) === false || rangeMonthPatternRegex.test(inputMonth.value) === false ) {
+            labelMonth.style.color = 'var(--light-red)'
+            inputMonth.classList.add('error')
+            errorFormatMessage[1].style.display = 'block'
+        } 
+
+        if (numberPatternRegex.test(inputYear.value) === false || Number(inputYear.value) > currentYear ) {
+            labelYear.style.color = 'var(--light-red)'
+            inputYear.classList.add('error')
+            errorFormatMessage[2].style.display = 'block'
+        } 
+    }
+
     const calculateAge = () => {
-        const currentDate = new Date().getMilliseconds()
+        const currentDate = new Date()
         
     }
+
+    const handleSubmit = () => {
+        
+
+        btn.addEventListener('click', event => {
+            event.preventDefault()
+            checkInputs()
+            checkFormat()
+        })
+    }
+    handleSubmit()
 }
 transformDate()
